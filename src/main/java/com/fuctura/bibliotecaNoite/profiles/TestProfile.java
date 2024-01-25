eea4b572aa6f836eeb92abad7453b1b63162a661
norbertodavid <norbertonex@gmail.com>
2024-01-25 19:43:26 -0300

    Comit 2

--- src/main/java/com/fuctura/bibliotecaNoite/profiles/TestProfile.java
+++ src/main/java/com/fuctura/bibliotecaNoite/profiles/TestProfile.java
@@ -6,14 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration //Para sinalizar ao projeto que esta é um perfil de teste do código.
+@Configuration //Serve para dizer que esta config deve rodar
 @Profile("test") //aqui sinalizamos qual perfil rodar no servidor.
 public class TestProfile {
 
     @Autowired
     private DBService dbService;  //Instaciamos o DB Service aqui.
 
-    @Bean  //Mapeamos o código abaixo e deixamos ele executável.
+    @Bean  //Executa o @Configuration e o DBService que estiver ativo no properties
     public void instanciaDB() {
         this.dbService.instanciaDB();
     }
